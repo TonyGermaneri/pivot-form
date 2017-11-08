ed3920ba4341dd576ba67e520c59f7a7eb94d084
Tony Germaneri <tgermaneri@fandango.com>
2017-11-08 00:59:59 -0800

    updated dist, close method

--- dist/pivot-form.js
+++ dist/pivot-form.js
@@ -1108,6 +1108,7 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*jslint browser
                 }
             });
             intf.open = open;
+            intf.close = close;
             intf.maximize = maximize;
             intf.minimize = minimize;
             intf.maximize = maximize;
